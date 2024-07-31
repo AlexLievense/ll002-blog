9751857d132af4eb19f74cda5dffdf60e54fe903
Alex Lievense <alexlievense@gmail.com>
2024-07-31 19:26:29 +0200

    feat: add initial Post migration, model, factory and seed logic

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->realTextBetween(50, 100),
+            'body' => fake()->realTextBetween(500, 2000),
+        ];
+    }
+}
