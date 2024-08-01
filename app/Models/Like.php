57565c0933b695a535130f91ebf2e8ab6293b0f6
Alex Lievense <alexlievense@gmail.com>
2024-08-01 20:32:34 +0200

    feat: add Like resources and relationships

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -5,24 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Comment extends Model
+class Like extends Model
 {
     use HasFactory;
-
-    public function post(): BelongsTo
-    {
-        return $this->belongsTo(Post::class);
-    }
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function likes(): HasMany
+    public function comment(): BelongsTo
     {
-        return $this->hasMany(Like::class);
+        return $this->belongsTo(Comment::class);
     }
 }
